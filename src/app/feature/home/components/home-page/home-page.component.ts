541f08fcf9498a0772ec5642172b5a7ec40c6f1a
penrodlol <penrod.christian7984@gmail.com>
2020-07-26 12:42:37 -0400

    [HOME] Responsive * Larger banner for xs | sm (ex. mobile). * Removing some padding for xs | sm.

--- src/app/feature/home/components/home-page/home-page.component.ts
+++ src/app/feature/home/components/home-page/home-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { MediaObserverService } from '@shared/media-observer/media-observer.service';
+import { Breakpoints } from '@shared/enums/breakpoints.enum';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
+  isSmaller$: Observable<boolean> = this.mediaObserverService.query([Breakpoints.XS, Breakpoints.SM]);
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private mediaObserverService: MediaObserverService
+  ) { }
 
 }
