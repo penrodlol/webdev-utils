d9d3d06a265921dcf59abd870d67a122f6efdfa2
penrodlol <penrod.christian7984@gmail.com>
2020-07-25 20:55:43 -0400

    [GENERAL] Sidenav Styling * Closed on xs | sm. * Position 'over' on xs | sm.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,32 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { WebDevUtilsRoutes } from '@routes/routes';
 
 import { IAuthUserState } from '@shared/state/auth/auth-user.state';
+import { MediaObserverService } from '@shared/media-observer/media-observer.service';
+import { Breakpoints } from '@shared/enums/breakpoints.enum';
 
 import { AuthUserActions } from '@auth/actions';
 
 import { Store } from '@ngrx/store';
 
-import { takeWhile, filter, take } from 'rxjs/operators';
+import { filter, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MediaObserverService]
 })
 export class AppComponent implements OnInit {
-  routes = WebDevUtilsRoutes;
   authUser$: Observable<firebase.User> = this.afAuth.user;
+  showSidenav$: Observable<boolean> = this.mediaObserverSevice.query([Breakpoints.MD, Breakpoints.LG, Breakpoints.XL]);
+
+  routes = WebDevUtilsRoutes;
 
   constructor(
     private store: Store<IAuthUserState>,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private mediaObserverSevice: MediaObserverService
   ) { }
 
   ngOnInit(): void {
