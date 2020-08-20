e729e8f641e9b3ed3a46ed2b8b1dc3fdd6083542
penrodlol <penrod.christian7984@gmail.com>
2020-08-20 18:10:48 -0400

    [GENERAL] Snackbar Styling * Snackbar background | color | border.

--- src/app/shared/snackbar/snackbar.service.ts
+++ src/app/shared/snackbar/snackbar.service.ts
@@ -5,14 +5,23 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   providedIn: 'root'
 })
 export class SnackbarService {
-  readonly BASE_DURATION = 3000;
 
-  constructor(private snackbar: MatSnackBar) { }
+  constructor(
+    private snackbar: MatSnackBar
+  ) { }
 
-  triggerSnackBar(message: string, action: string = null, duration: number = this.BASE_DURATION) {
-    this.snackbar.open(message, action, {
-      duration,
-      verticalPosition: 'top'
-    });
+  triggerSnackBar(message: string, action?: 'Dismiss' | 'Ok', duration?: number) {
+    this.snackbar.open(
+      message,
+      action,
+      {
+        duration: duration ? duration : 3000,
+        verticalPosition: 'top',
+        panelClass: [
+          'snackbar',
+          `snackbar_action-${!!action}`
+        ]
+      }
+    );
   }
 }
